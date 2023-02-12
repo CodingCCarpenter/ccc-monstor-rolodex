299e6a149b773bc54b3efd809185ce2975e99380
CodingCCarpenter <christineassists@aol.com>
2023-02-12 16:19:15 -0600

    update setState in onClick on button to use both firstName and lastName to show shallow merge

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi! My name is {this.state.name} and I work at {this.state.company}.
+            Hi! My name is {this.state.name.firstName} {this.state.name.lastName} and I work at {this.state.company}.
           </p>
           <button onClick={() => {
-            this.setState({name: 'Christine'});
+            this.setState({name: {firstname: 'Christine', lastName: 'Carpenter'}});
           }}> 
             Change Name
           </button>
