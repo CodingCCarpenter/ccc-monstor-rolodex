98fd5bff981c35a9a676ea99e64a799655781079
CodingCCarpenter <christineassists@aol.com>
2023-02-13 20:25:12 -0600

    add basic search functionality by creating a new filteredMonsters array and setting that as the state

--- src/App.js
+++ src/App.js
@@ -24,8 +24,21 @@ class App extends Component {
       ));
   }
   render() {
+    console.log('render')
     return (
       <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => {
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.includes(event.target.value);
+            });
+            this.setState(() => {
+              return { monsters: filteredMonsters }
+            })
+            }}/>
         {
           this.state.monsters.map((monster) => {
             return(
