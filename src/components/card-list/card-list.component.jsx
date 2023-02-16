3daa43e1c2ac2cd65aa5c19253afe37db6c55d49
codingccarpenter <christineassists@aol.com>
2023-02-16 09:36:51 -0800

    add basic structure for monster card

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -9,8 +9,13 @@ class CardList extends Component {
             <div>
                 {monsters.map((monster) => {
                     return(
-                    <div key={monster.id}>
-                        <h1>{monster.name}</h1>
+                    <div className='card-container' key={monster.id}>
+                       <img 
+                            alt={`monster ${monster.name}`} 
+                            src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+                        />
+                        <h2>{monster.name}</h2>
+                        <p>{monster.email}</p>
                     </div>
                     );
                 })}
