432923c48d2637baa95e00c5f7c9b02afcfef85e
codingccarpenter <christineassists@aol.com>
2023-02-18 16:38:01 -0600

    convert searchbox to functional component

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,26 +1,41 @@
 import { Component } from "react";
 import './search-box.style.css';
 
-class SearchBox extends Component {
-
-    render () {
-        const {monsters, searchField} = this.props
-        const filteredMonsters = monsters.filter((monster) => {
-            return monster.name.toLowerCase().includes(searchField);
-        });
-
-
-        return(
-            <div>
-                <input 
-                    className={this.props.className}
-                    type='search' 
-                    placeholder={this.props.placeholder} 
-                    onChange={this.props.onChangeHandler}
-                />
-            </div>
-        )
-    }
+const SearchBox = ({className, placeholder, onChangeHandler}) => {
+    return(
+        <div>
+            <input 
+                className={className}
+                type='search' 
+                placeholder={placeholder} 
+                onChange={onChangeHandler}
+            />
+        </div>
+    )
 }
+
+
+
+// class SearchBox extends Component {
+
+//     render () {
+//         const {monsters, searchField} = this.props
+//         const filteredMonsters = monsters.filter((monster) => {
+//             return monster.name.toLowerCase().includes(searchField);
+//         });
+
+
+//         return(
+//             <div>
+//                 <input 
+//                     className={this.props.className}
+//                     type='search' 
+//                     placeholder={this.props.placeholder} 
+//                     onChange={this.props.onChangeHandler}
+//                 />
+//             </div>
+//         )
+//     }
+// }
 
 export default SearchBox;
