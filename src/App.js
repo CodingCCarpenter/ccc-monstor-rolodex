2c1a5eb29394b8cfd453861209a77901f4f403de
codingccarpenter <christineassists@aol.com>
2023-02-18 16:09:57 -0600

    add additional useEffect to control how often filteredMonsters is run

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters, setFilteredMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -18,14 +18,22 @@ const App = () => {
     .then((users) => setMonsters(users));
   }, []);
 
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+  
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
   }
 
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLowerCase().includes(searchField);
-  });
+  // filteredMonsters = monsters.filter((monster) => {
+  //   return monster.name.toLowerCase().includes(searchField);
+  // });
 
   return(
     <div className="App">
